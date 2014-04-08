77518138b242df41b670a200f4d95356b464ad19
Joshua Ledbetter <Ledbetterljoshua@gmail.com>
2014-04-07 20:32:53 -0500

    added gem 'will_paginate-bootstrap'

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,15 +4,15 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order ("created_at DESC").paginate(:page => params[:page])
   end
 
   def show
   end
 
-  def new
-    @pin = current_user.pins.build
-  end
+ def index
+   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
+ end
 
   def edit
   end
