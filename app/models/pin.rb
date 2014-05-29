0a378abc0c7fb692d801658d722008e3f2e9222b
Joshua Ledbetter <Ledbetterljoshua@gmail.com>
2014-05-28 22:34:00 -0500

    added next and previous buttons

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,15 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
+	def previous
+  Pin.where(["id < ?", id]).order(:id).last
+	end
+
+	def next
+  Pin.where(["id > ?", id]).order(:id).first
+	end
+
 	has_attached_file :image, :styles => { :medium => "700x700>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
 end
+
